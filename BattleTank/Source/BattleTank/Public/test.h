cbd692110c5f8417de0a2ee918454e355e61b25a
KTS-20160508KDA\Administrator <375922584@qq.com>
2018-06-20 16:08:41 +0800

    完成

---
+++ BattleTank/Source/BattleTank/Public/test.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "WidgetBlueprint.h"
+#include "test.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_API Utest : public UWidgetBlueprint
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
